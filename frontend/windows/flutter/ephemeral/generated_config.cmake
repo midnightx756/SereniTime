39e0926256b83414e03fd9b564ec668525839ae9
midnightx756 <midnightphantomrst@gmail.com>
2025-03-22 15:29:42 +0530

    Add files via upload

---
+++ frontend/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "F:\\LOL\\textures\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "F:\\LOL\\workout\\Pyproject\\Frontend" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=F:\\LOL\\textures\\flutter"
+  "PROJECT_DIR=F:\\LOL\\workout\\Pyproject\\Frontend"
+  "FLUTTER_ROOT=F:\\LOL\\textures\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=F:\\LOL\\workout\\Pyproject\\Frontend\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=F:\\LOL\\workout\\Pyproject\\Frontend"
+  "FLUTTER_TARGET=F:\\LOL\\workout\\Pyproject\\Frontend\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=F:\\LOL\\workout\\Pyproject\\Frontend\\.dart_tool\\package_config.json"
+)
